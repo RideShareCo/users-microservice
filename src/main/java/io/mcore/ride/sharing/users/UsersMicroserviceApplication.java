d7283e2da1b9033021d171e8076841e5dc297749
Mario Cuellar <mariocuellar1@gmail.com>
2016-11-06 21:20:02 +0100

    Seguridad oAuth

--- src/main/java/io/mcore/ride/sharing/users/UsersMicroserviceApplication.java
+++ src/main/java/io/mcore/ride/sharing/users/UsersMicroserviceApplication.java
@@ -2,8 +2,10 @@ package io.mcore.ride.sharing.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class UsersMicroserviceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/mcore/ride/sharing/users/controller/UsersController.java
+++ src/main/java/io/mcore/ride/sharing/users/controller/UsersController.java
@@ -1,6 +1,8 @@
 package io.mcore.ride.sharing.users.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,14 +11,37 @@ import io.mcore.ride.sharing.users.dao.UsersDAO;
 import io.mcore.ride.sharing.users.model.User;
 
 @Controller
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class UsersController {
-	
-	@Autowired UsersDAO userDao;
-	
+
+	@Autowired
+	UsersDAO userDao;
+
+	@PreAuthorize("#oauth2.hasScope('openid')")
 	@RequestMapping("/users")
 	@ResponseBody
 	public Iterable<User> getUsers() {
 		return userDao.findAll();
 	}
+
+	@RequestMapping("/users2")
+	@ResponseBody
+	public Iterable<User> getUsers2() {
+		return userDao.findAll();
+	}
+
+	/*
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.requestMatchers().antMatchers("/users").and().authorizeRequests().antMatchers("/users")
+			.access("#oauth2.hasScope('openid')").and().requestMatchers().antMatchers("/users2").and().authorizeRequests().antMatchers("/users2")
+			.authenticated();
+		}
+
+	}*/
 
 }
